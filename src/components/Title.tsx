bd28cd9adf7e1554d0fe56433f5851861d4b7201
Owethu Sotomela <78469603+OwethuSotomela@users.noreply.github.com>
2025-06-14 20:15:27 +0200

    Update Title.tsx

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
 
+const Title = ({ children }: { children: string }) => {
+  return <Text style={styles.title}>{children}</Text>;
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 10
+  }
+});
+
+export default Title;
